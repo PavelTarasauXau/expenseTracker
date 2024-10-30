ef10d03eaa3af1acbbfcd670f2651158749197a9
Pavel Tarasau <zefirfricadelkovich@gmail.com>
2024-10-31 01:35:12 +0300

    account class

---
+++ Account.java
@@ -0,0 +1,30 @@
+public class Account {
+    private double balance;
+
+    public Account(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
