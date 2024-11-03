bcdf3ccf8f1d951de141db790cc0dd8645f2d50f
Pavel Tarasau <zefirfricadelkovich@gmail.com>
2024-11-04 02:39:08 +0300

    User and Main updated

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        User user = new User("Pavel", "12345678");
+        User user = new User("Pavel", "12345678", 100.0); // Инициируем пользователю начальный баланс
 
         while (true) {
             System.out.print("Input login: ");
@@ -14,13 +14,17 @@ public class Main {
 
             if (user.authorize(log, pass)) {
                 System.out.println("Successfully logged in!");
+
+                user.getAccount().deposit(50.0);
+                System.out.println("Current balance: " + user.getAccount().getBalance());
+                user.getAccount().withdraw(20.0);
+                System.out.println("Current balance: " + user.getAccount().getBalance());
                 break;
             } else {
                 System.out.println("Try again");
             }
         }
+
         in.close();
     }
 }
-
-
--- User.java
+++ User.java
@@ -1,10 +1,12 @@
 public class User {
-    private String login;
-    private String password;
+    private final String login;
+    private final String password;
+    private final Account account; // Включаем объект Account в User
 
-    public User(String login, String password) {
+    public User(String login, String password, double initialBalance) {
         this.login = login;
         this.password = password;
+        this.account = new Account(initialBalance);
     }
 
     public String getLogin() {
@@ -15,7 +17,10 @@ public class User {
         return password;
     }
 
-    // Метод для авторизации
+    public Account getAccount() {
+        return account; // Метод для получения счета
+    }
+
     public boolean authorize(String inputLogin, String inputPassword) {
         return this.login.equals(inputLogin) && this.password.equals(inputPassword);
     }
